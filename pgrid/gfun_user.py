335a4f1d58b7ebc7c93d98a94dc133889729370e
Parker MacCready <p.maccready@gmail.com>
2021-11-12 17:02:52 -0800

    Update gfun_user.py

--- pgrid/gfun_user.py
+++ pgrid/gfun_user.py
@@ -12,26 +12,28 @@ pth = Path(__file__).absolute().parent.parent.parent / 'LO' / 'pgrid'
 if str(pth) not in sys.path:
     sys.path.append(str(pth))
 import gfun_utility as gfu
+import gfun
 
 # This is the name of the grid that you are working on.
-gridname = 'ai0'
+gridname = 'so0'
 
 # default s-coordinate info (could override below)
 s_dict = {'THETA_S': 4, 'THETA_B': 2, 'TCLINE': 10, 'N': 30,
         'VTRANSFORM': 2, 'VSTRETCHING': 4}
 
-if gridname in ['ai0','hc0', 'sal0']:
+if gridname in ['ai0','hc0', 'sal0', 'so0']:
     # These are the gridname and tag to feed to use when creating the Ldir paths.
     # They are used for accessing the river tracks, which may be developed for one
     # grid but reused in others.
     base_gridname = 'cas6'
     base_tag = 'v3'
 
-def make_initial_info(dch, gridname=gridname):
+def make_initial_info(gridname=gridname):
     # Add an elif section for your grid.
 
     if gridname == 'sal0':
         # A Salish Sea grid, used as an example.
+        dch = gfun.default_choices()
         aa = [-124, -122, 47, 49]
         res = 600 # target resolution (m)
         Lon_vec, Lat_vec = gfu.simple_grid(aa, res)
@@ -52,6 +54,7 @@ def make_initial_info(dch, gridname=gridname):
             z = z + dch['z_offset']
             
     elif gridname == 'hc0':
+        dch = gfun.default_choices()
         aa = [-123.2, -122.537, 47.3, 47.9]
         res = 100 # target resolution (m)
         Lon_vec, Lat_vec = gfu.simple_grid(aa, res)
@@ -75,6 +78,7 @@ def make_initial_info(dch, gridname=gridname):
             z = z + dch['z_offset']
             
     elif gridname == 'ai0':
+        dch = gfun.default_choices()
         aa = [-122.82, -122.36, 47.758, 48.18]
         res = 100 # target resolution (m)
         Lon_vec, Lat_vec = gfu.simple_grid(aa, res)
@@ -96,6 +100,33 @@ def make_initial_info(dch, gridname=gridname):
             z[~np.isnan(z_part)] = z_part[~np.isnan(z_part)]
         if dch['use_z_offset']:
             z = z + dch['z_offset']
+            
+    elif gridname == 'so0':
+        # South Sound
+        dch = gfun.default_choices(wet_dry=True)
+        dch['z_land'] = 4
+        aa = [-123.13, -122.76, 47, 47.42]
+        res = 50 # target resolution (m)
+        Lon_vec, Lat_vec = gfu.simple_grid(aa, res)
+        dch['t_list'] = [dch['t_dir'] / 'psdem_10m' / 'PS_30m.nc']
+        dch['nudging_edges'] = ['east']
+        dch['nudging_days'] = (0.1, 1.0)
+        
+        # Make the rho grid.
+        lon, lat = np.meshgrid(Lon_vec, Lat_vec)
+        # initialize the bathymetry array
+        z = np.nan * lon
+        # add bathymetry automatically from files
+        for t_fn in dch['t_list']:
+            print('\nOPENING BATHY FILE: ' + t_fn.name)
+            tlon_vec, tlat_vec, tz = gfu.load_bathy_nc(t_fn)
+            tlon, tlat = np.meshgrid(tlon_vec, tlat_vec)
+            z_part = zfun.interp2(lon, lat, tlon, tlat, tz)
+            # put good values of z_part in z
+            z[~np.isnan(z_part)] = z_part[~np.isnan(z_part)]
+        if dch['use_z_offset']:
+            z = z + dch['z_offset']
+    
         
         
     else:
