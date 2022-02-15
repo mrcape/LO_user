45a40301db7a3584382f1da1fb65d867fb5d9484
Parker MacCready <p.maccready@gmail.com>
2022-02-14 16:57:56 -0800

    Update gfun_user.py

--- pgrid/gfun_user.py
+++ pgrid/gfun_user.py
@@ -4,7 +4,8 @@ User-specific code for pgrid.
 You would edit the information to reflect whatever grid you are working on.
 """
 import numpy as np
-from lo_tools import zfun
+import pandas as pd
+from lo_tools import zfun, Lfun
 
 import sys
 from pathlib import Path
@@ -15,7 +16,7 @@ import gfun_utility as gfu
 import gfun
 
 # This is the name of the grid that you are working on.
-gridname = 'so0'
+gridname = 'ae0'
 
 # default s-coordinate info (could override below)
 s_dict = {'THETA_S': 4, 'THETA_B': 2, 'TCLINE': 10, 'N': 30,
@@ -27,6 +28,9 @@ if gridname in ['ai0','hc0', 'sal0', 'so0']:
     # grid but reused in others.
     base_gridname = 'cas6'
     base_tag = 'v3'
+elif gridname in ['ae0']:
+    base_gridname = 'ae0'
+    base_tag = 'v0'
 
 def make_initial_info(gridname=gridname):
     # Add an elif section for your grid.
@@ -104,15 +108,8 @@ def make_initial_info(gridname=gridname):
     elif gridname == 'so0':
         # South Sound
         dch = gfun.default_choices()
-        
         dch['z_offset'] = -1.3 # NAVD88 is 1.3 m below MSL at Seattle
-        
-        #dch['min_depth'] = 0.4 # requires WET_DRY in ROMS
-        
         dch['excluded_rivers'] = ['skokomish']
-        
-        #dch['rx0max'] = 0.12 # default is 0.15 (smaller = smoother)
-        
         aa = [-123.13, -122.76, 47, 47.42]
         res = 50 # target resolution (m)
         Lon_vec, Lat_vec = gfu.simple_grid(aa, res)
@@ -120,7 +117,6 @@ def make_initial_info(gridname=gridname):
                     dch['t_dir'] / 'psdem_10m' / 'PS_30m.nc']
         dch['nudging_edges'] = ['east']
         dch['nudging_days'] = (0.1, 1.0)
-        
         # Make the rho grid.
         lon, lat = np.meshgrid(Lon_vec, Lat_vec)
         # initialize the bathymetry array
@@ -135,7 +131,51 @@ def make_initial_info(gridname=gridname):
             z[~np.isnan(z_part)] = z_part[~np.isnan(z_part)]
         if dch['use_z_offset']:
             z = z + dch['z_offset']
-    
+            
+    elif gridname == 'ae0':
+        # idealized model estuary
+        dch = gfun.default_choices()
+        lon_list = [-2, 0, 1, 2, 3]
+        x_res_list = [2500, 500, 500, 2500, 2500]
+        lat_list = [43, 44.9, 45.1, 47]
+        y_res_list = [2500, 500, 500, 2500]
+        Lon_vec, Lat_vec = gfu.stretched_grid(lon_list, x_res_list,
+                                            lat_list, y_res_list)
+        lon, lat = np.meshgrid(Lon_vec, Lat_vec)
+        dch['analytical'] = True
+        dch['nudging_edges'] = ['north', 'south', 'west']
+        dch['use_z_offset'] = False
+        # tidy up dch
+        dch['z_offset'] = 0.0
+        dch['t_dir'] = 'BLANK'
+        dch['t_list'] = ['BLANK']
+        # make bathymetry by hand
+        z = np.zeros(lon.shape)
+        x, y = zfun.ll2xy(lon, lat, 0, 45)
+        zshelf = x * 1e-3
+        zestuary = -20 + 20*x/1e5 + 20/(1e4)*np.abs(y)
+        z = zshelf
+        mask = zestuary < z
+        z[mask] = zestuary[mask]
+        
+        # create a river file
+        Ldir = Lfun.Lstart(gridname=base_gridname, tag=base_tag)
+        ri_dir = Ldir['LOo'] / 'pre' / 'river' / Ldir['gtag']
+        Lfun.make_dir(ri_dir)
+        gri_fn = ri_dir / 'river_info.csv'
+        with open(gri_fn, 'w') as rf:
+            rf.write('rname,usgs,ec,nws,ratio,depth,flow_units,temp_units\n')
+            rf.write('creek0,,,,1.0,5.0,m3/s,degC\n')
+        # and make a geographic path
+        track_dir = ri_dir / 'tracks'
+        Lfun.make_dir(track_dir)
+        track_fn = track_dir / 'creek0.p'
+        track_df = pd.DataFrame()
+        track_df['lon'] = np.linspace(0,4,100)
+        track_df['lat'] = 45*np.ones(100)
+        track_df.to_pickle(track_fn)
+        # NOTE: tracks go from ocean to land
+        
         
         
     else:
